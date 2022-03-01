9edecc7621bc2276dbbdbbd1dda53ba9f6a8d71c
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-03-01 11:04:51 +0200

    commit

--- src/main/java/com/collectionsfirst/collectionsfirst/CollectionsfirstApplication.java
+++ src/main/java/com/collectionsfirst/collectionsfirst/CollectionsfirstApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CollectionsfirstApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CollectionsfirstApplication.class, args);
 	}
